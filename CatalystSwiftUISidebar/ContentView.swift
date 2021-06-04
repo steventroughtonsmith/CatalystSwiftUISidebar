d331b255c09ff06e02213c3e905c48903ac48308
Steven Troughton-Smith <steventroughtonsmith@gmail.com>
2021-06-04 19:09:47 +0100

    Initial checkin

---
+++ CatalystSwiftUISidebar/ContentView.swift
@@ -0,0 +1,86 @@
+//
+//  ContentView.swift
+//  CatalystSwiftUISidebar
+//
+//  Created by Steven Troughton-Smith on 04/06/2021.
+//
+
+import SwiftUI
+
+
+struct SidebarSplitView: View, UIViewControllerRepresentable {
+	
+	typealias UIViewControllerType = UISplitViewController
+	let splitViewController = UISplitViewController(style: .tripleColumn)
+	
+	var columnA = UIViewController()
+	var columnB = UIViewController()
+	var columnC = UIViewController()
+	
+	init<A:View, B:View, C:View>(@ViewBuilder content: @escaping () -> TupleView <(A,B,C)>) {
+		
+		columnA = UIHostingController(rootView: content().value.0)
+		columnB = UIHostingController(rootView: content().value.1)
+		columnC = UIHostingController(rootView: content().value.2)
+		
+		columnA.view.backgroundColor = .clear
+		columnB.view.backgroundColor = .clear
+		columnC.view.backgroundColor = .clear
+		
+		splitViewController.viewControllers = [columnA, columnB, columnC]
+	}
+	
+	func makeUIViewController(context: Context) -> UIViewControllerType {
+		splitViewController.primaryBackgroundStyle = .sidebar
+		splitViewController.preferredDisplayMode = .twoBesideSecondary
+		
+		return splitViewController
+	}
+	
+	func updateUIViewController(_ uiView: UIViewControllerType, context: Context) {
+		
+	}
+}
+
+struct ContentView: View {
+	var body: some View {
+		SidebarSplitView {
+			List {
+				Section(header: Text("Library")) {
+					HStack{
+						Image(systemName: "doc")
+						Text("All")
+					}
+					HStack{
+						Image(systemName: "clock")
+						Text("Recents")
+					}
+				}
+			}
+			.listStyle(PlainListStyle())
+			.navigationBarHidden((UIDevice.current.userInterfaceIdiom == .mac))
+			
+			List {
+				Text("Item 1")
+				Text("Item 2")
+				Text("Item 3")
+			}
+			.navigationBarHidden((UIDevice.current.userInterfaceIdiom == .mac))
+			
+			Group {
+				Text("Hello, world!")
+					.padding()
+			}
+			.navigationBarHidden((UIDevice.current.userInterfaceIdiom == .mac))
+			
+		}
+		.ignoresSafeArea()
+	}
+	
+}
+
+struct ContentView_Previews: PreviewProvider {
+	static var previews: some View {
+		ContentView()
+	}
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# CatalystSwiftUISidebar
+
+Really trivial example showing how to wrap SwiftUI in a UISplitViewController, so that you can use the correct sidebar background style when building for macOS.
+
+N.B. the built-in SidebarListStyle does not behave correctly on macOS, so if you want to look like a Mac app you'll need to have your own, custom list style, or use some other kind of control to populate your sidebar
+
+### Screenshot
+
+![](https://hccdata.s3.us-east-1.amazonaws.com/gh_catalystswiftuisidebar.jpg)
